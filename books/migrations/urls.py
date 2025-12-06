ae031eb46406892070dd2b92e97dc4c4de4bb178
Othniel Powell <othniel@live.ca>
2025-12-05 20:09:11 -0500

    Initial Django bookstore project setup

---
+++ books/migrations/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('book/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+]
+
